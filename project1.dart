2cf90cbc98b369b4797b2338bc473abdfd2be062
Mudassir138 <163653997+Mudassir138@users.noreply.github.com>
2024-03-16 18:12:54 +0500

    Add files via upload

---
+++ project1.dart
@@ -0,0 +1,17 @@
+//Muddasar Mehmood
+void main(){
+ num a = 13;
+ num b = 7;
+ num sum = a+b;
+ num diff = a-b;
+ num mul = a*b;
+ num div = a/b;
+
+ print("Number 1 = $a");
+ print("Number 2 = $b");
+ print("The Sum of Two Number : $sum");
+ print("The difference of Two Number : $diff");
+ print("The product of Two Number : $mul");
+ print("The quotient of Two Number : $div");
+
+}
